0c2bb24d4ced16cfad91af5b6463c29bdd70fc88
wapernato <98657698+wapernato@users.noreply.github.com>
2025-10-14 22:20:22 +0300

    Add files via upload

---
+++ Greeting.java
@@ -0,0 +1,14 @@
+
+
+package main.java.com.Task2_1;
+
+public class Greeting {
+    public Greeting() { }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+}
+
+
+
