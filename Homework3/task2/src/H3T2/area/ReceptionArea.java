6b1195064dd746bd40696cca0eaf7aa7cff91528
wapernato <rodion080797@gmail.com>
2025-11-09 16:13:12 +0300

    task2

---
+++ Homework3/task2/src/H3T2/area/ReceptionArea.java
@@ -0,0 +1,13 @@
+package H3T2.area;
+
+public class ReceptionArea {
+    private final int load;
+
+    public ReceptionArea(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+}
