e7a3126c12a5b5f7759821c1410b06bc02c25f7a
wapernato <rodion080797@gmail.com>
2025-11-10 10:03:54 +0300

    -

--- task2/AdministrationDepartment/src/com/senla/service/SortStats.java
+++ task2/AdministrationDepartment/src/com/senla/service/SortStats.java
@@ -9,18 +9,13 @@ public interface SortStats {
     void freeSortRoomByCapacity(Rooms rooms);
     void freeSortRoomByPrice(Rooms rooms);
 
-    // (1) «Список постояльцев и их номеров (сортировка: имя, затем дата выезда)»
     void sortGuestsByAlphabetThenCheckout(GuestRegistry guests);
 
-    // (4) детали отдельного номера
     void printRoomDetails(Rooms rooms, String roomId, GuestRegistry guests);
 
-    // (5) «Цены услуг и номеров (сортировать по разделу, цене)»
     void printPrices(Rooms rooms, ServiceCatalog services);
 
-    // (6) услуги постояльца (sortByPrice=true -> по цене; false -> по дате)
     void printGuestServices(String guestName, ServiceUsageRegistry usage, boolean sortByPrice);
 
-    // (7) 3 последних постояльца номера
     void printLast3GuestsOfRoom(String roomId, GuestRegistry guests);
 }
