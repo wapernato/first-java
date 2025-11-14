034043e140e0368adb611a8804c6c17496ddb140
wapernato <rodion080797@gmail.com>
2025-11-14 15:36:15 +0300

    Сделал понятные выводы + task2 подкорректировал

--- Homework3/task2/HierarchyLayout/src/main/java/com/senla/Warehouse.java
+++ Homework3/task2/HierarchyLayout/src/main/java/com/senla/Warehouse.java
@@ -1,20 +1,22 @@
 package H3T2;
 
-import H3T2.area.AllArea;
-import H3T2.area.ReceptionArea;
-import H3T2.area.ShippingArea;
+import H3T2.goods.Product;
+
+import java.util.List;
 
 public class Warehouse {
+
+    private static final double MAX_CAPACITY_KG = 200.0;
+
     public static void main(String[] args) {
-        ReceptionArea reception = new ReceptionArea(2);
-        ShippingArea shipping = new ShippingArea(3);
 
-        AllArea all = new AllArea(
-                reception.getLoad(),
-                shipping.getLoad()
-        );
+        // Заполняем склад
+        List<Product> storedProducts = WarehouseUtils.fillWarehouse(MAX_CAPACITY_KG);
 
-        int answer = all.AllLoad();
-        System.out.println(answer);
+        // Считаем общий вес
+        double totalWeight = WarehouseUtils.getTotalWeight(storedProducts);
+
+        // Печатаем отчёт
+        WarehouseUtils.printReport(storedProducts, totalWeight, MAX_CAPACITY_KG);
     }
 }
