8259d3c9f5f7c831ea193c13716191a09782433b
wapernato <rodion080797@gmail.com>
2025-11-23 21:54:16 +0300

    hw6

---
+++ Homework6/task1/AdministrationDepartment/src/main/java/com/senla/service/impl/ImportFiles.java
@@ -0,0 +1,92 @@
+package com.senla.service.impl;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+
+import com.senla.service.GuestRegistry;
+import com.senla.service.Rooms;
+import com.senla.service.WorksWithFilesImport;
+
+public class ImportFiles implements WorksWithFilesImport {
+    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private final Rooms rooms;
+    private final GuestRegistry guest;
+
+    public ImportFiles(Rooms rooms, GuestRegistry guest) {
+        this.rooms = rooms;
+        this.guest = guest;
+    }
+
+    public void importGuest(Path path){
+        try (BufferedReader read = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = read.readLine()) != null) {
+                String[] str = line.split("\\s*[,;]\\s*");
+
+                String number = str[0];
+                String name = str[1];
+                LocalDate date = LocalDate.parse(str[2], fmt);
+                int nights = Integer.parseInt(str[3]);
+                int id = Integer.parseInt(str[4]);
+                guest.addHuman(number, name, date, nights);
+
+            }
+        }catch (IOException e) {
+            System.out.println("Ошибка чтения файла: " + path);
+            throw new RuntimeException(e);
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Некорректный формат данных в файле: " + path);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void importRooms(Path path) {
+        try (BufferedReader read = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = read.readLine()) != null) {
+                String[] str = line.split("\\s*[,;]\\s*");
+
+                String numer = str[0];
+                int capacity = Integer.parseInt(str[1]);
+                int stars = Integer.parseInt(str[2]);
+                int id = Integer.parseInt(str[3]); // пока не используется
+
+                rooms.addRoom(numer, capacity, stars);
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка чтения файла: " + path);
+            throw new RuntimeException(e);
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Некорректный формат данных в файле: " + path);
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void importService(Path path){
+        try (BufferedReader read = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = read.readLine()) != null){
+                String[] str = line.split("\\s*,\\s*");
+                String nameService = str[0];
+                int price = Integer.parseInt(str[1]);
+            }
+        }catch (IOException e) {
+            System.out.println("Ошибка чтения файла: " + path);
+            throw new RuntimeException(e);
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Некорректный формат данных в файле: " + path);
+            throw new RuntimeException(e);
+        }
+    }
+
+}
