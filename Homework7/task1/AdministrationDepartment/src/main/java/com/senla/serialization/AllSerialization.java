b5b668b6bebf9e399736e4094f929ee272127c5a
wapernato <rodion080797@gmail.com>
2025-12-06 00:40:20 +0300

    Add HW7 serialization implementation

---
+++ Homework7/task1/AdministrationDepartment/src/main/java/com/senla/serialization/AllSerialization.java
@@ -0,0 +1,46 @@
+package com.senla.serialization;
+
+import com.senla.service.GuestRegistry;
+import com.senla.service.Rooms;
+import com.senla.service.ServiceCatalog;
+import com.senla.service.ServiceUsageRegistry;
+
+public class AllSerialization {
+
+    private final GuestRegistry guests;
+    private final Rooms rooms;
+    private final ServiceCatalog catalog;
+    private final ServiceUsageRegistry usage;
+
+    private final SerializationRooms serializationRooms;
+    private final SerializationGuestRegistry serializationGuestRegistry;
+    private final SerializationServiceCatalog serializationServiceCatalog;
+    private final SerializationServiceUsageRegistry serializationServiceUsageRegistry;
+
+    public AllSerialization(GuestRegistry guests,
+                            Rooms rooms,
+                            ServiceCatalog catalog,
+                            ServiceUsageRegistry usage) {
+        this.guests = guests;
+        this.rooms = rooms;
+        this.catalog = catalog;
+        this.usage = usage;
+
+        this.serializationRooms = new SerializationRooms(rooms);
+        this.serializationGuestRegistry = new SerializationGuestRegistry(guests);
+        this.serializationServiceCatalog = new SerializationServiceCatalog(catalog);
+        this.serializationServiceUsageRegistry = new SerializationServiceUsageRegistry(usage);
+    }
+
+    public void serializationAll() {
+        serializationRooms.serializationAllRooms();
+        serializationRooms.serializationAllRoomsNextId();
+        serializationGuestRegistry.serializeAllGuestEntries();
+        serializationGuestRegistry.serializeGuestNextId();
+        serializationServiceCatalog.serializationGetService();
+        serializationServiceCatalog.serializationGetServiceNextId();
+        serializationServiceUsageRegistry.serializationAllServiceUsage();
+        serializationServiceUsageRegistry.serializationAllServiceUsageNextId();
+        System.out.println("=== СЕРИАЛИЗАЦИЯ ДАННЫХ ЗАВЕРШЕНА ===");
+    }
+}
---
+++ Homework7/task1/AdministrationDepartment/src/main/java/com/senla/serialization/SerializationServiceCatalog.java
@@ -0,0 +1,42 @@
+package com.senla.serialization;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.senla.service.GuestRegistry;
+import com.senla.service.ServiceCatalog;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SerializationServiceCatalog {
+
+    private final ObjectMapper mapper = new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    private final ServiceCatalog catalog;
+
+    public SerializationServiceCatalog(ServiceCatalog catalog) {
+
+        this.catalog = catalog;
+    }
+
+
+    public void serializationGetService(){
+        try(FileWriter writer = new FileWriter("C:\\Users\\wapernato\\CoursesHomework\\Homework7\\serialization\\GetServicePrice.txt")) {
+            String json = mapper.writeValueAsString(catalog.getService());
+            writer.write(json);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void serializationGetServiceNextId(){
+        try(FileWriter writer = new FileWriter("C:\\Users\\wapernato\\CoursesHomework\\Homework7\\serialization\\GetServicePriceNextId.txt")) {
+            String json = mapper.writeValueAsString(catalog.getNextId());
+            writer.write(json);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
