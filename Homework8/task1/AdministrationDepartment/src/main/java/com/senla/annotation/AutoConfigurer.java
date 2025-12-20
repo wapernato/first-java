88a89c93adea7138a36b77fd6e7a2926bcc8554e
wapernato <rodion080797@gmail.com>
2025-12-20 04:22:02 +0300

    hw8

--- Homework8/task1/AdministrationDepartment/src/main/java/com/senla/annotation/AutoConfigurer.java
+++ Homework8/task1/AdministrationDepartment/src/main/java/com/senla/annotation/AutoConfigurer.java
@@ -19,7 +19,7 @@ public class AutoConfigurer {
                 String fileName = ann.configFileName();
                 String propertyName = ann.propertyName();
                 Class<?> classType = ann.type();
-                if(fileName.isEmpty()){
+                if(fileName.isEmpty()){ //  если не указано имя файла/пустой файл, то берем наш файл
                     fileName = DEFAULT_CONFIG_FILE;
                 }
                 if(propertyName.isEmpty()){
@@ -31,11 +31,40 @@ public class AutoConfigurer {
                 }
                 if(classType == Object.class){
                     classType = String.class;
+
                 }
 
 
-                try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))){
+                try (BufferedReader br = new BufferedReader(new FileReader(fileName))){
+                    String ln;
+                    while ((ln = br.readLine()) != null){
+                        String[] line = ln.trim().split("\\s*=\\s*", 2);
+                        String key = line[0];
+                        String value = line[1].trim();
+                        if(key.equals(propertyName)){
+                            if(classType == Integer.class || classType == int.class){
+                                Integer valueInteger = Integer.parseInt(value);
+                                field.set(target, valueInteger);
+                            }
+                            else if(classType == Boolean.class || classType == boolean.class){
+                                Boolean valueBoolean = Boolean.parseBoolean(value);
+                                field.set(target, valueBoolean);
+                            }
+                            else if (classType == long.class || classType == Long.class) {
+                                Long valueLong = Long.parseLong(value);
+                                field.set(target, valueLong);
+                            }
+                            else if(classType == double.class || classType == Double.class){
+                                Double valueDouble = Double.parseDouble(value);
+                                field.set(target, valueDouble);
+                            }
+                            else if(classType == String.class){
+                                field.set(target, value);
+                            }
 
+                            break;
+                        }
+                    }
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
