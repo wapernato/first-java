fc8fcf9274e46704e742b03fb07820ca8b3f694b
wapernato <rodion080797@gmail.com>
2026-01-05 15:27:37 +0300

    Merge hw6/Rodion

---
+++ Homework6/task1/AdministrationDepartment/src/main/java/com/senla/service/Rooms.java
@@ -0,0 +1,34 @@
+package com.senla.service;
+
+
+import com.senla.model.Room;
+import com.senla.model.RoomStatus;
+
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+
+public interface Rooms {
+    //int getId();
+
+    Set<String> getRoomsNumbers();
+    Set<String> freeRoomsNumber();
+    Set<Integer> getRoomId();
+
+    //List<String> freeRooms();
+    void addRoom(String number, int capacity, int stars, double price);
+    int countCapacity();
+    int count();
+
+    void setRoomStatus(String number, RoomStatus status);
+    void setRoomPrice(String number, double price);
+    void setRoomStars(String number, int stars);
+    void setRoomCapacity(String number, int capacity);
+    //void getAllStatusRoom(String number);
+
+    Room getRoom(String number);
+    boolean isRoomBookable(int id);
+}
