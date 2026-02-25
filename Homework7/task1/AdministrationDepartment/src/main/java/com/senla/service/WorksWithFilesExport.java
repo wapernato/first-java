4672f703a92b6b8b71959352798900234546b513
wapernato <rodion080797@gmail.com>
2026-02-25 11:33:39 +0300

    hw8

--- Homework7/task1/AdministrationDepartment/src/main/java/com/senla/service/WorksWithFilesExport.java
+++
@@ -1,9 +0,0 @@
-package com.senla.service;
-
-import java.nio.file.Path;
-
-public interface WorksWithFilesExport {
-    void exportGuest(Path path);
-    void exportService(Path path);
-    void exportRooms(Path path);
-}
